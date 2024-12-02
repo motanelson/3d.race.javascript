2f09e32b3bbe92d72bddb5993091727c99014762
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-02 09:34:53 +0000

    Add files via upload

--- sum.py
+++ sum.py
@@ -1,15 +1,14 @@
-def sums(list1,list2):
+def sums(list1):
     list3=[]
+    total=0
     for count in range(len(list1)):
         value=0
         value1=list1[count]
-        value2=list2[count]
-        value=value1 + value2
-        list3=list3+[value]
+        
+        total=total + value1
+        list3=list3+[total]
     return list3    
 l1=[10,41,20,221]
-l2=[112,41,21,211]
-l3=sums(l1,l2)
+l3=sums(l1)
 print(l1)
-print(l2)
 print(l3)
